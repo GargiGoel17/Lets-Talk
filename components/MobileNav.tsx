46fdbf237ffd1df2981c4f4fd386fba8ea0f631c
GargiGoel17 <gargigoel17@gmail.com>
2024-07-06 00:01:03 +0530

    Fixing a production issue

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -31,7 +31,7 @@ const MobileNav = () => {
               height={32}
               alt="letstalk logo"
             />
-            <p className="text-[26px] font-extrabold text-white">Let's Talk</p>
+            <p className="text-[26px] font-extrabold text-white">Lets Talk</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
